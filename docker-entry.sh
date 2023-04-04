82e09c2883f82add6796ee36282f4204c52e97c8
Justin Wong <vsajustinwong@gmail.com>
2023-04-04 14:25:35 -0700

    changed projection as argument

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./bin/opensfm_run_all $1
-./bin/opensfm export_geocoords $1 --dense --proj '+proj=latlong +ellps=WGS84 +datum=WGS84'
+./bin/opensfm export_geocoords $1 --dense --proj $2
